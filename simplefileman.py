bf43e8f8962d99d4d272363442ca53221e59d675
Tyler Lorence <105127051+TylerLorence@users.noreply.github.com>
2022-05-13 14:01:55 -0400

    Squash
    Test Commit
    
    This is a test commit.
    
    Add help command
    
    Implement simple help command and add "exit" to command_information dict
    
    Update simplefileman.py

--- simplefileman.py
+++ simplefileman.py
@@ -1,16 +1,18 @@
 import os
+import sys
+import ctypes
 import logging
 
 
 """
 Tyler Lorence's Project #1 (Untitled)
-Version Pre-Indev (SNAPSHOT 3 - 75 MINUTE MARK)
+Version Pre-Indev (SNAPSHOT 4.2)
 
 TODO: Add error handling with Try/Except.
 TODO: Add error logging with the logging module.
 TODO: Add 'appendfile' command
-TODO: Add 'help' command
 TODO: Other ideas that come to me along the way.
+TODO: Add 'filesize' command
 
 
 """
@@ -92,6 +94,28 @@ def write_to_file(*contents):
         logger.error(f"Error attempting to write to file: Cannot find the file {contents[0][1]}")
 
 
+
+command_information = {
+    ("help", "?"): "--- Command \"help\" / \"?\" ---\n\nReturns the list of commands, or information about a command\nUsage: help [command name]\nExample: help\nExample: help cd\n",
+    ("dir", "ls"): "--- Command \"dir\" / \"ls\" ---\n\nReturns the elements of the directory the program is currently in\nUsage: dir\nExample: 'dir'\n",
+    ("cd"): "--- Command \"cd\" ---\n\nChanges the directory of the program\nUsage: cd <new directory>\nExample: 'cd C:\\Users\\admin\\Desktop'\nExample: 'cd Users'\n",
+    ("md", "mkdir"): "--- Command \"md\" / \"mkdir\" ---\n\nMakes a new directory\nUsage: 'md <New Folder Name>'\nExample: 'md NewFolder'\n",
+    ("rd", "rmdir"): "--- Command \"rd\" / \"rmdir\" ---\n\nRemoves a directory\nUsage: 'rd <Folder to Remove>'\nExample: 'rd NewFolder'\n",
+    ("createfile", "makefile"): "--- Command \"createfile\" / \"makefile\" ---\n\nCreates a new file\nUsage: createfile <new file name>\nExample: 'createfile HelloWorld.txt'\n",
+    ("read", "more"): "--- Command \"read\" / \"more\" ---\n\nReads the text contents of a file.\nUsage: read <file name>\nExample: 'read HelloWorld.txt'\n",
+    ("write", "writeto"): "--- Command \"write\" / \"writeto\" ---\n\nWrites text content to a file, overriding the current file contents\nUsage: write <file name> <new file contents>\nExample: 'write HelloWorld.txt Hello, world!'\n",
+    ("append", "appendto"): "--- Command \"append\" / \"appendto\" ---\n\nDEVELOPER NOTE: This command has not been made yet! Appends text content to a file, appending to the current file contents\nUsage: append <file name> <file contents to append>\nExample: 'append HelloWorld.txt Hi!!'\n",
+    ("copy", "copyfile"): "--- Command \"copy\" / \"copyfile\" ---\n\nDEVELOPER NOTE: This command has not been made yet! Copies a file.\nUsage: copy <file to copy> <new file>\nExample: 'copy HelloWorld.txt TestFile.txt'\n",
+    ("del", "delete"): "--- Command \"del\" / \"delete\" ---\n\nDeletes a file\nUsage: del <file to delete>\nExample: 'del TestFile.txt'\n",
+    ("filesize"): "--- Command \"help\" / \"?\" ---\n\nDEVELOPER NOTE: This command has not been made yet! Gets the size of a file.\nUsage: filesize <file name>\nExample: 'filesize HelloWorld.txt'",
+    ("exit"): "--- Command \"exit\" ---\n\nExits the program\nUsage: exit\nExample: 'exit'\n"
+
+
+}
+
+
+
+
 while True:
     user_input = input(f"{os.getcwd()}>").split(" ")
     if user_input[0].casefold() == "ls" or user_input[0].casefold() == "dir":
@@ -101,7 +125,12 @@ while True:
     elif user_input[0].casefold() == "createfile":
         create_file(user_input[1])
     elif user_input[0].casefold() == "md" or user_input[0].casefold() == "mkdir":
-        make_directory(user_input[1])
+        try:
+            make_directory(user_input[1])
+        except IndexError:
+            logger.error("Error attempting to create directory: No directory name provided!")
+        except FileExistsError:
+            logger.error(f"Error attempting to make directory: Cannot create a directory when that directory already exists: '{user_input[1]}'")
     elif user_input[0].casefold() == "rd" or user_input[0].casefold() == "rmdir":
         while True:
             confirmation = input(f"WARNING: Are you sure you want to remove the directory \"{user_input[1]}\"? This action is permanent and cannot be undone. (Y/N)\n> ")
@@ -135,10 +164,32 @@ while True:
         except IndexError:
             logger.error(f"Error attempting to write to file: No filename was provided.")
     elif user_input[0].casefold() == "help" or user_input[0].casefold() == "?":
-        print("This still needs to be added!")
+        if len(user_input) == 1:
+            print("""\
+                Available Commands:
+                help, dir, cd, md, rd, createfile, read, write, append, copy, del, filesize
+                Type 'help <command name> to get information about a command.
+                """)
+        else:
+            # print(command_information.get(user_input[1], "Error: Command Not Found!"))
+            found = 0
+            for key, value in command_information.items():
+                if user_input[1].casefold() in key:
+                    found = 1
+                    print(value)
+                    break
+                else:
+                    pass
+            if found == 0:
+                logger.error(f"Error attempting to get information about command {user_input[1]}: Command Not Found!")
+            
+            
     elif user_input[0].casefold() == "exit":
         exit(0)
+    elif user_input[0].casefold() == "debug_uac":
+        # This is a debug command that will be removed in the future. Its purpose is to test opening a new instance of the program with UAC permissions.
+        # The documentation is based on the ShellExecuteA function, seen in Microsoft's documentation for the Win32 Shell API.
+        # https://docs.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-shellexecutea?redirectedfrom=MSDN
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
     else:
         print("Invalid command!")
-
-
